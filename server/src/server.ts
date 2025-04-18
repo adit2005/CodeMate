08f66e534aeed9090dc925790da6d308c419ea11
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-04-18 20:39:15 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "public"))) // Serve static files
 const server = http.createServer(app)
 const io = new Server(server, {
 	cors: {
-		origin: ["https://code-mate-hcc6.vercel.app/", "http://localhost:5173"],
+		origin: ["https://code-mate-hcc6.vercel.app", "http://localhost:5173"],
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
 		credentials: true
