99d0e485dbc14ee48900bad5acf97bf73d466b91
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-04-18 19:58:51 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-	origin: ["https://code-mate-hcc6-palv1t6lb-adit2005s-projects.vercel.app", "http://localhost:5173"],
+	origin: ["https://code-mate-hcc6.vercel.app", "http://localhost:5173"],
 	credentials: true,
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization']
